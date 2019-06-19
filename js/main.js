a5a08ef5fc1fb09fd10de4f917e1703c96476761
uo <schoolgirluche@gmail.com>
2019-06-18 23:38:58 -0700

    fixed subnav

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
+
 jQuery(document).ready(function($){
 	var mainHeader = $('.cd-auto-hide-header'),
 		secondaryNavigation = $('.cd-secondary-nav'),
 		//this applies only if secondary nav is below intro section
-		belowNavHeroContent = $('.sub-nav-hero'),
+		belowNavHeroContent = $('.process'),
 		headerHeight = mainHeader.height();
-	
+
 	//set scrolling variables
 	var scrolling = false,
 		previousTop = 0,
@@ -34,7 +35,7 @@ jQuery(document).ready(function($){
 	function autoHideHeader() {
 		var currentTop = $(window).scrollTop();
 
-		( belowNavHeroContent.length > 0 ) 
+		( belowNavHeroContent.length > 0 )
 			? checkStickyNavigation(currentTop) // secondary navigation below intro
 			: checkSimpleNavigation(currentTop);
 
@@ -56,9 +57,9 @@ jQuery(document).ready(function($){
 	function checkStickyNavigation(currentTop) {
 		//secondary nav below intro section - sticky secondary nav
 		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
-		
+
 		if (previousTop >= currentTop ) {
-	    	//if scrolling up... 
+	    	//if scrolling up...
 	    	if( currentTop < secondaryNavOffsetTop ) {
 	    		//secondary nav is not fixed
 	    		mainHeader.removeClass('is-hidden');
@@ -67,19 +68,19 @@ jQuery(document).ready(function($){
 	    	} else if( previousTop - currentTop > scrollDelta ) {
 	    		//secondary nav is fixed
 	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
+	    		secondaryNavigation.removeClass('slide-up').addClass('fixed');
 	    		belowNavHeroContent.addClass('secondary-nav-fixed');
 	    	}
-	    	
+
 	    } else {
-	    	//if scrolling down...	
+	    	//if scrolling down...
 	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
 	 	  		//hide primary nav
 	    		mainHeader.addClass('is-hidden');
 	    		secondaryNavigation.addClass('fixed slide-up');
 	    		belowNavHeroContent.addClass('secondary-nav-fixed');
 	    	} else if( currentTop > secondaryNavOffsetTop ) {
-	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
+	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset
 	    		mainHeader.removeClass('is-hidden');
 	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
 	    		belowNavHeroContent.addClass('secondary-nav-fixed');
@@ -115,4 +116,6 @@ $(function(){  // $(document).ready shorthand
 	  
 	  });
 	  
-  })});
+  }
+
+  )})
