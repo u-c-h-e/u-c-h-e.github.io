83fc78e7fb7fd4308a7552dab84acc7a42dd265c
uo <schoolgirluche@gmail.com>
2019-06-29 20:46:29 -0700

    final product

--- js/main.js
+++ js/main.js
@@ -1,5 +1,40 @@
 
 jQuery(document).ready(function($){
+	var nativeWidth = 0;
+	var nativeHeight = 0;
+
+	$(".magnifierWrap").mousemove(function(e) {
+		if(!nativeWidth && !nativeHeight) {
+			var imgObject = new Image();
+			imgObject.src = $(".smallImg").attr("src");
+			nativeWidth = imgObject.width;
+			nativeHeight = imgObject.height;
+		}
+		else {
+			var magnifyOffset = $(this).offset();
+			var mouseX = e.pageX - magnifyOffset.left;
+			var mouseY = e.pageY - magnifyOffset.top;
+			
+			if(mouseX < $(this).width() && mouseY < $(this).height() && mouseX > 0 && mouseY > 0) {
+				$(".magnifier").fadeIn(100);
+			}
+			else{
+				$(".magnifier").fadeOut(100);
+			}
+			if($(".magnifier").is(":visible")){
+				var smallImgX = Math.round(mouseX/$(".smallImg").width()*nativeWidth - $(".magnifier").width()/2)*-1;
+				var smallImgY = Math.round(mouseY/$(".smallImg").height()*nativeHeight - $(".magnifier").height()/2)*-1;
+				var bgp = smallImgX + "px " + smallImgY + "px";
+				
+				var largeImgX = mouseX - $(".magnifier").width()/2;
+				var largeImgY = mouseY - $(".magnifier").height()/2;
+				
+				$(".magnifier").css({left: largeImgX, top: largeImgY, backgroundPosition: bgp});
+			}
+		}
+	});
+	
+
 	var mainHeader = $('.cd-auto-hide-header'),
 		secondaryNavigation = $('.cd-secondary-nav'),
 		//this applies only if secondary nav is below intro section
@@ -94,17 +129,20 @@ jQuery(document).ready(function($){
 	    }
 	}
 
-	var h = document.documentElement,
-  b = document.body,
-  st = 'scrollTop',
-  sh = 'scrollHeight',
-  progress = document.querySelector('.progress'),
-  scroll;
+
+var h = document.documentElement,
+    b = document.body,
+    st = 'scrollTop',
+    sh = 'scrollHeight',
+    progress = document.querySelector('.progress'),
+    scroll;
 
 document.addEventListener('scroll', function() {
   scroll = (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100;
   progress.style.setProperty('--scroll', scroll + '%');
 });
+
+
 
 
 
@@ -136,4 +174,7 @@ $(function(){  // $(document).ready shorthand
 	  
   }
 
+  
+
+  
   )})
