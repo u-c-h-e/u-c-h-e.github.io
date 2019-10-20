2f4f087ac990ee6810fab991513a167b7d807226
uo <schoolgirluche@gmail.com>
2019-10-20 15:40:20 -0700

    pw

--- js/testimonial.js
+++ js/testimonial.js
@@ -19,7 +19,7 @@
 // 	});
 // 	arrows[1].addEventListener('click', sliderEvent);
 // 	arrows[1].addEventListener('autoClick', sliderEvent);
-	
+
 // 	function sliderEvent(e) {
 // 		e = e || window.event;
 // 		e.preventDefault();
@@ -30,7 +30,7 @@
 // 			scrollInterval = setInterval(autoScroll, interval);
 // 		}
 // 	}
-	
+
 // 	function slider() {
 // 		switch (carouselCount) {
 // 			case -100:
@@ -47,14 +47,14 @@
 // 			slides[i].setAttribute('style', 'transform:translateX(-' + carouselCount + '%)');
 // 		}
 // 	}
-	
+
 // 	// create new Event to dispatch click for auto scroll
 // 	var autoClick = new Event('autoClick');
 // 	function autoScroll() {
 // 		arrows[1].dispatchEvent(autoClick);
 // 	}
-	
+
 // 	// set timing of dispatch click events
 // 	scrollInterval = setInterval(autoScroll, interval);
-	
+
 // })();
